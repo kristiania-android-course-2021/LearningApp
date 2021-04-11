0d0747ca60d1870a9464a92353d368edc1f7b47b
Imran Baig <imran.baig@vipps.no>
2021-04-11 18:47:30 +0200

    Updated for the live coding

--- app/src/main/java/com/imran/appname/MainActivity.kt
+++ app/src/main/java/com/imran/appname/MainActivity.kt
@@ -3,21 +3,15 @@ package com.imran.appname
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
-import com.imran.appname.datastorage.RaceDatabase
-import com.imran.appname.viewmodel.MainViewModel
 
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var viewModel : MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        viewModel = MainViewModel(this)
 
         var button = findViewById<Button>(R.id.buttonAddRecord)
 
@@ -28,15 +22,4 @@ class MainActivity : AppCompatActivity() {
             }
         }
     }
-
-    override fun onResume() {
-        super.onResume()
-
-        viewModel.getRecords().observe(this, { list->
-            list.map {entity->
-                Log.d(this.javaClass.simpleName, entity.toString())
-            }
-        })
-    }
-
 }
--- app/src/main/java/com/imran/appname/RecordActivity.kt
+++ app/src/main/java/com/imran/appname/RecordActivity.kt
@@ -5,44 +5,29 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import com.imran.appname.databinding.ActivityRecordBinding
-import com.imran.appname.datastorage.RaceDatabase
-import com.imran.appname.datastorage.RecordEntity
-import com.imran.appname.viewmodel.RecordViewModel
+
 
 class RecordActivity : AppCompatActivity() {
 
     lateinit var binding: ActivityRecordBinding
-    private lateinit var viewModel : RecordViewModel
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         binding = ActivityRecordBinding.inflate(layoutInflater)
-        viewModel = RecordViewModel(this)
 
         setContentView(binding.root)
 
         binding.buttonSave.setOnClickListener {
 
-            var entity = RecordEntity(
-                binding.editName.text.toString(),
-                binding.editLength.text.toString().toInt(),
-                binding.editTime.text.toString().toInt()
-            )
-
-            //This is the non blocking way of accessing database
-            viewModel.addRaceRecord(entity)
-
+            //Todo Add record to database
             finish() // This will finish this activity. app will navigate back to main activity.
         }
 
         binding.editName.addTextChangedListener(textWatcher)
         binding.editLength.addTextChangedListener(textWatcher)
         binding.editTime.addTextChangedListener(textWatcher)
-
-
-
+        
     }
 
 
